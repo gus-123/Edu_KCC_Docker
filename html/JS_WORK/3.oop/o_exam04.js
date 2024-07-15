36d2bf2de0ea3a5eb1981f1e560959b0fae67702
gus-123 <hyunmin1711@gmail.com>
2024-07-15 10:06:53 +0900

    commit 24/07/15

--- html/JS_WORK/3.oop/o_exam04.js
+++ html/JS_WORK/3.oop/o_exam04.js
@@ -32,10 +32,57 @@ class IdolModel2 {
   get name() {
     return this.#name;
   }
+
+  set year(year) {
+    this.#year = year;
+  }
+
+  get year() {
+    return this.#year;
+  }
+
+  // class에 포함됨 그래서 class 형식으로 호춯해주어야 함.
+  static returnGroupName() {
+    return '아이브';  // static method
+  }
 }
 
 let yuJin = new IdolModel2('안유진', 2003);
 //console.log(yuJin.name);  // private로 선언하여  undifined
 
 yuJin.name = '장원영';
-console.log(yuJin.name); 
+yuJin.year = '24/07/15';
+console.log(yuJin.name +' ' + yuJin.year);  
+console.log(IdolModel2.returnGroupName());
+
+/**
+ * factory constructor pattern
+ */
+
+class IdolModel3 {
+  name;
+  year;
+
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+  }
+
+  static fromObject(object) { // '파라미터.변수명' 형식
+    return new IdolModel3(object.name, object.year);
+  }
+
+  static fromList(list) { // 배열 형식으로 받아 idolModel3 인스턴스로 생성
+    return new IdolModel3(list[0], list[1]);
+  }
+}
+
+// 객체 추가
+const yuJin2 = IdolModel3.fromObject({ name: '안유진', year: 2003})
+
+console.log(yuJin2);
+
+// 배열 추가
+
+const wonYoung = IdolModel3.fromList(['장원영', '2004']);
+console.log(wonYoung);
